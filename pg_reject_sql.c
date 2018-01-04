eb10e6ceabbfc763b4d28dc90836c6fcf3df9f79
kostiantyn.nemchenko <kostiantyn.nemchenko@betlab.com>
2018-01-04 15:57:26 +0200

    Fix identation

--- pg_reject_sql.c
+++ pg_reject_sql.c
@@ -13,27 +13,27 @@ static ProcessUtility_hook_type prev_utility_hook = NULL;
 
 static void
 reject_sql(PlannedStmt *pstmt,
-		   const char *queryString,
-		   ProcessUtilityContext context,
-		   ParamListInfo params,
+           const char *queryString,
+           ProcessUtilityContext context,
+           ParamListInfo params,
            QueryEnvironment *queryEnv,
-		   DestReceiver *dest,
-		   char *completionTag)
+           DestReceiver *dest,
+           char *completionTag)
 {
     Node *parsetree = pstmt->utilityStmt;
-
-	switch (nodeTag(parsetree))
-	{
+    
+    switch (nodeTag(parsetree))
+    {
         /* Catch ALTER SYSTEM statement */
-		case T_AlterSystemStmt:
-		{
+        case T_AlterSystemStmt:
+        {
             const char *current_user = GetUserNameFromId(GetUserId(), false);
-
+            
             if (strcmp(current_user, ALLOWED_USER) != 0)
             {
                 ereport(ERROR,
-			            (errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
-			             errmsg("user %s is not permitted to execute ALTER SYSTEM",
+                        (errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
+                         errmsg("user %s is not permitted to execute ALTER SYSTEM",
                                 current_user)));
             }
             return;
@@ -41,26 +41,26 @@ reject_sql(PlannedStmt *pstmt,
         default:
             break;
     }
-
-	if (prev_utility_hook)
+    
+    if (prev_utility_hook)
     {
-		(*prev_utility_hook) (pstmt, queryString,
+        (*prev_utility_hook) (pstmt, queryString,
                               context, params, queryEnv,
                               dest, completionTag);
     }
-	else
-	{
-    	standard_ProcessUtility(pstmt, queryString,
+    else
+    {
+        standard_ProcessUtility(pstmt, queryString,
                                 context, params, queryEnv,
                                 dest, completionTag);
     }
 }
 
-void
+void 
 _PG_init(void)
 {
     prev_utility_hook = ProcessUtility_hook;
-	ProcessUtility_hook = reject_sql;
+    ProcessUtility_hook = reject_sql;
 }
 
 void
